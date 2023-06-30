ceadb64d81613a061282f57bb76b1f3a7c2ed3d2
roger <zrj564366@126.com>
2023-06-30 18:35:23 +0800

    correct host patch impl

--- testPatchMatch.m
+++ testPatchMatch.m
@@ -26,11 +26,10 @@ subplot(2,2,4);imshow(imresize(cur_patch, [480 640]));
 
 
 
-check_id = 39;
-% load('G:\matlab\LK\slam_algo\patch_match.mat');
-load('G:\matlab\LK\slam_algo\patch_match2.mat');
-% check_id = 43;
-% load('G:\matlab\LK\slam_algo\patch_match3.mat');
+
+% check_id = 39;host_id = 1;target_id = 8;load('G:\matlab\LK\slam_algo\patch_match.mat');
+check_id = 39;host_id = 1;target_id = 2;load('G:\matlab\LK\slam_algo\patch_match2.mat');
+% check_id = 43;host_id = 1;target_id = 2; load('G:\matlab\LK\slam_algo\patch_match3.mat');
 % load('G:\matlab\LK\slam_algo\pba_large_scene_2.mat');
 
 
@@ -90,8 +89,7 @@ for i = 0 : length(LR)-1
 end
 
 
-host_id = 1;
-target_id = 2;8;
+
 
 host_img = imgaussfilt(rgb2gray(LR{host_id, 1}), 0.6);
 target_img = imgaussfilt(rgb2gray(LR{target_id, 1}), 0.6);
@@ -124,7 +122,7 @@ gy(2 : height-1, :) = 0.5 * (double(host_img(3:height,:)) - double(host_img(1:he
 
 
 max_levels = 5;
-level_ref = 2;0;1;
+level_ref = 2; 1;2;0;1;
 halfpatch_size = 4;
 depth = depths(check_id);
 depth_ref = depth;
@@ -150,9 +148,16 @@ intrMat_scaled(2,3) = (intrMat(2,3)-0.5) /2 +0.5;
 pc_cur_scaled = pflat(intrMat_scaled * target_xyz(check_id,:)');
 pc_cur_scaled = pc_cur_scaled(1:2)';
 pc_cur_check_error = 2*(pc_cur_scaled-0.5)+0.5 - pc_cur_check;
+
+
+% is_host_square = false;
 WarpAffine(A_th, host_img, px_ref, level_ref, target_img, pc_cur_check, search_level, halfpatch_size+1);
 
-
+% is_host_square = true;
+% WarpAffine(inv(A_th), target_img, pc_cur_check, search_level, host_img, px_ref, level_ref, halfpatch_size+1);
+[A_th_scaled, ~] = GetWarpMatrixAffineInv(intrMat, px_ref, level_ref, halfpatch_size, T_th, xyz_ref);
+search_level_scaled = GetBestSearchLevel(inv(A_th_scaled), max_levels - 1);
+WarpAffineInv(A_th, host_img, px_ref, level_ref, target_img, pc_cur_check, search_level, halfpatch_size+1);
 
 
 end
@@ -170,17 +175,13 @@ function WarpAffine(A_cur_ref, host_img, px_ref, level_ref, target_img, px_cur, 
 patch_size = halfpatch_size * 2;
 patch_size_use = (halfpatch_size-1) * 2;
 A_ref_cur = inv(A_cur_ref);
-
 host_img0 = imresize(host_img, size(host_img)* 2^(-level_ref));
 target_img0 = imresize(target_img, size(target_img)* 2^(-search_level));
-
 px_ref_ = (px_ref-0.5) * 2^(-level_ref) + 0.5;
 [xMat, yMat] = meshgrid(0:patch_size-1, 0:patch_size-1);
 pix0 = [xMat(:) yMat(:)];
-
 pix0 = pix0-halfpatch_size;
 % pix = (pix0 -0.5) * 2^(search_level) + 0.5;
-
 % svo 2.0 not
 if 1
     pix = (pix0 ) * 2^(search_level);
@@ -192,14 +193,10 @@ end
 pix_ = (A_ref_cur * pix')' + px_ref_;       % 缩放尺度
 % patch_bound = (A_ref_cur * pix')' + (px_ref_-0.5) * 2^(search_level) + 0.5; % 原尺寸
 patch_bound = (A_ref_cur * pix')' + (px_ref_-0.5) * 2^(level_ref) + 0.5; % 原尺寸
-
 warped_patchs = interp2(double(host_img0), pix_(:, 1), pix_(:, 2), 'linear', 0);
-
 check_id = 3;
 x_final = bilinearInterp(double(host_img0), pix_(check_id,:), floor(pix_(check_id,1)), ceil(pix_(check_id,1)), floor(pix_(check_id,2)), ceil(pix_(check_id,2)));
 error = x_final - warped_patchs(check_id,:);
-
-
 
 %   warped_patch = [];
 %   pixs = [];
@@ -210,15 +207,60 @@ error = x_final - warped_patchs(check_id,:);
 %     end
 %   end
 
-
 px_cur_scaled = (px_cur -0.5) * 2^(-search_level) + 0.5;
 px_curs = px_cur + pix0;
 px_curs_scaled = px_cur_scaled + pix0;
-
 figure,subplot(2,2,1);imshow(host_img0);hold on;plot(pix_(:,1), pix_(:,2),'.r');plot(px_ref_(:,1), px_ref_(:,2),'.g');title(sprintf('small image, covers more area, patch src, level: %d',level_ref));
 subplot(2,2,2);imshow(target_img0);hold on;plot(px_curs_scaled(:,1), px_curs_scaled(:,2),'.r');plot(px_cur_scaled(:,1), px_cur_scaled(:,2),'.g');title(sprintf('target patch at search level: %d', search_level));
 subplot(2,2,3);imshow(host_img);hold on;plot(patch_bound(:,1), patch_bound(:,2),'.r');plot(px_ref(:,1), px_ref(:,2),'.g');title('orig host image, covers less area, level: 0');
 subplot(2,2,4);imshow(target_img);hold on;plot(px_curs(:,1), px_curs(:,2),'.r');plot(px_cur(:,1), px_cur(:,2),'.g');title('orig target image, covers less area, level: 0');
+
+
+end
+function WarpAffineInv(A_cur_ref, host_img, px_ref, level_ref, target_img, px_cur, search_level, halfpatch_size)
+patch_size = halfpatch_size * 2;
+patch_size_use = (halfpatch_size-1) * 2;
+% A_cur_ref = inv(A_ref_cur);
+host_img0 = imresize(host_img, size(host_img)* 2^(-level_ref));
+target_img0 = imresize(target_img, size(target_img)* 2^(-search_level));
+px_cur_ = (px_cur-0.5) * 2^(-search_level) + 0.5;
+[xMat, yMat] = meshgrid(0:patch_size-1, 0:patch_size-1);
+pix0 = [xMat(:) yMat(:)];
+pix0 = pix0-halfpatch_size;
+% pix = (pix0 -0.5) * 2^(search_level) + 0.5;
+% svo 2.0 not
+if 1
+    pix = (pix0 ) * 2^(-search_level);
+else
+    pix = pix0;
+end
+% pix = (pix0 ) * 2^(level_ref);
+% pix = (pix0 -0.5) * 2^(0) + 0.5;
+pix_ = (A_cur_ref * pix')' + px_cur_;       % 缩放尺度
+patch_bound = (A_cur_ref * pix')' + (px_cur_-0.5) * 2^(search_level) + 0.5; % 原尺寸
+warped_patchs = interp2(double(target_img0), pix_(:, 1), pix_(:, 2), 'linear', 0);
+check_id = 3;
+x_final = bilinearInterp(double(target_img0), pix_(check_id,:), floor(pix_(check_id,1)), ceil(pix_(check_id,1)), floor(pix_(check_id,2)), ceil(pix_(check_id,2)));
+error = x_final - warped_patchs(check_id,:);
+
+%   warped_patch = [];
+%   pixs = [];
+%   for  y = 2 : patch_size_use + 1
+%     ref_patch_border_ptr = patch_with_border_ + y * (patch_size_ + 2) + 1;
+%     for x = 1 : patch_size_use
+%       ref_patch_ptr[x] = ref_patch_border_ptr[x];
+%     end
+%   end
+
+px_host_scaled = (px_ref -0.5) * 2^(-level_ref) + 0.5;
+px_hosts = px_ref + pix0;
+px_hosts_scaled = px_host_scaled + pix0;
+figure,subplot(2,2,1);imshow(host_img0);hold on;plot(px_hosts_scaled(:,1), px_hosts_scaled(:,2),'.r');plot(px_host_scaled(:,1), px_host_scaled(:,2),'.g');title(sprintf('small image, covers more area, patch src, level: %d',level_ref));
+subplot(2,2,2);imshow(target_img0);hold on;plot(pix_(:,1), pix_(:,2),'.r');plot(px_cur_(:,1), px_cur_(:,2),'.g');title(sprintf('target patch at search level: %d', search_level));
+subplot(2,2,3);imshow(host_img);hold on;plot(px_hosts(:,1), px_hosts(:,2),'.r');plot(px_ref(:,1), px_ref(:,2),'.g');title('orig host image, covers less area, level: 0');
+subplot(2,2,4);imshow(target_img);hold on;plot(patch_bound(:,1), patch_bound(:,2),'.r');plot(px_cur(:,1), px_cur(:,2),'.g');title('orig target image, covers less area, level: 0');
+
+
 
 end
 function x_final = bilinearInterp(Lut, proj_pinhole, proj_pinhole_x1, proj_pinhole_x2, proj_pinhole_y1, proj_pinhole_y2)
@@ -260,6 +302,42 @@ is_fisheye = 0;1;
 
 xyz_du_ref = inv(intrMat) * [px_ref + 2^level_ref * [halfpatch_size+1 0] 1]';
 xyz_dv_ref = inv(intrMat) * [px_ref + 2^level_ref * [0 halfpatch_size+1] 1]';
+
+
+if ~is_fisheye
+    % 假设patch内的z分量一样
+    xyz_du_ref1 = xyz_du_ref * xyz_ref(3) / xyz_du_ref(3);
+    xyz_dv_ref1 = xyz_dv_ref * xyz_ref(3) / xyz_dv_ref(3);
+    xyz_du_ref = xyz_du_ref1;
+    xyz_dv_ref = xyz_dv_ref1;
+else
+    % 假设patch内的xyz的模长一样
+    xyz_du_ref0 = xyz_du_ref./norm(xyz_du_ref);
+    xyz_dv_ref0 = xyz_dv_ref./norm(xyz_dv_ref);
+    xyz_du_ref2 = xyz_du_ref0 * depth_ref_norm;
+    xyz_dv_ref2 = xyz_dv_ref0 * depth_ref_norm;
+    xyz_du_ref = xyz_du_ref2;
+    xyz_dv_ref = xyz_dv_ref2;
+end
+
+px_cur = pflat(intrMat * (T_th(1:3,1:3)*xyz_ref' + T_th(1:3,4)));
+px_du = pflat(intrMat * (T_th(1:3,1:3)*xyz_du_ref + T_th(1:3,4)));
+px_dv = pflat(intrMat * (T_th(1:3,1:3)*xyz_dv_ref + T_th(1:3,4)));
+A_th = zeros(2,2);
+A_th(:,1) = (px_du(1:2) - px_cur(1:2))/(halfpatch_size+1);
+A_th(:,2) = (px_dv(1:2) - px_cur(1:2))/(halfpatch_size+1);
+end
+function [A_th, px_cur] = GetWarpMatrixAffineInv(intrMat, px_ref, level_ref, halfpatch_size, T_th, xyz_ref)
+
+is_fisheye = 0;1;
+
+if 0
+    xyz_du_ref = inv(intrMat) * [px_ref + 2^level_ref * [halfpatch_size+1 0] 1]';
+    xyz_dv_ref = inv(intrMat) * [px_ref + 2^level_ref * [0 halfpatch_size+1] 1]';
+else
+    xyz_du_ref = inv(intrMat) * [px_ref + 2^(-level_ref) * [halfpatch_size+1 0] 1]';
+    xyz_dv_ref = inv(intrMat) * [px_ref + 2^(-level_ref) * [0 halfpatch_size+1] 1]';
+end
 
 
 if ~is_fisheye
